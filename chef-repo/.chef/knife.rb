400bc65bfb9d41fc90b3b9d3b367c6de956f9d65
iKenji <jdabc1843@gmail.com>
2017-06-21 14:47:23 +0900

    update

---
+++ chef-repo/.chef/knife.rb
@@ -0,0 +1,8 @@
+local_mode true
+cookbook_path [
+  "/home/nginx/qnaj/chef-repo/cookbooks",
+  "/home/nginx/qnaj/chef-repo/site-cookbooks"
+]
+environment_path [
+  "/home/nginx/qnaj/chef-repo/environment"
+]
